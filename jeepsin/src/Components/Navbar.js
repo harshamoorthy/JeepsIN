46dbbe39d2a8b7cd8fff6badcb5cb94b8c35ff42
mpsingh009 <mpsinghj6@gmail.com>
2024-01-25 13:51:14 -0500

    added reac- bootstrap

--- jeepsin/src/Components/Navbar.js
+++ jeepsin/src/Components/Navbar.js
@@ -3,30 +3,25 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const NavbarComponent = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand href="#home">JeepsIN</Navbar.Brand>
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    {/* <input class="form-control mr-sm-2" type="search" placeholder="Search"> */}
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+        {/* Collapse button for small screens */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Navbar links */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            {/* Add more Nav.Link components for additional links */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 }
 
